339136527f840b678aee6c5b6df7bce4ed6cb508
constance khalumba <constancekhalumba@gmail.com>
2025-04-04 15:45:42 +0300

    last commits for the day

--- connie-portfolio-react/src/components/home/Data.jsx
+++ connie-portfolio-react/src/components/home/Data.jsx
@@ -4,8 +4,34 @@ const Data = () => {
   return (
      <div className="home__data">
         <h1 className="home__title">Constance Khalumba
-            
+        <svg
+    width="36"
+    height="36"
+    viewBox="0 0 48 48"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    className="home__hand"
+  >
+    <path
+      d="M25.4995 32.0305L31.3495 33.1555L36.1495 8.48051C36.4495 6.83051 35.3995 5.18051 33.8245 4.88051C32.1745 4.58051 30.5995 5.70551 30.2995 7.35551L25.4995 32.0305Z"
+      fill="#FFDD67"
+    />
+    <path
+      d="M33.8996 4.88018C33.6746 4.80518 33.5246 4.80518 33.2996 4.80518C34.6496 5.33018 35.3996 6.75518 35.0996 8.25518L30.2996 32.9302L31.3496 33.1552L36.1496 8.48018C36.5246 6.75518 35.4746 5.18018 33.8996 4.88018Z"
+      fill="#EBA352"
+    />
+    <path
+      d="M19.4995 32.7802H26.5495V5.55518C26.5495 3.53018 24.9745 1.80518 23.0245 1.80518C21.1495 1.80518 19.4995 3.45518 19.4995 5.55518V32.7802Z"
+      fill="#FFDD67"
+    />
+        </svg>        
         </h1>
+        <h3 className="home__subtitle">Web Developer</h3>
+        <p className="home___description">Hi, I'm Constance Khalumba, a passionate web developer creating innovative and user-friendly digital experiences.</p>
+
+        <a href="#contact" className="button button--flex"></a>
+        Say Hello
+        
      </div>
   )
 }
