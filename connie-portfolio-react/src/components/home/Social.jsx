0644f058f68af46875252e287a756449c119ca81
constance khalumba <constancekhalumba@gmail.com>
2025-04-04 15:07:08 +0300

    UPDATES ON THE ASSETS

---
+++ connie-portfolio-react/src/components/home/Social.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Social = () => {
+  return (
+   <div className="home__social">
+     <a href="" className="home__social-icon" target="_blank">
+     <i class="uil uil-instagram"></i>
+     </a>
+
+     <a href="https://github.com/Connie-Khalumba" className="home__social-icon" target="_blank">
+     <i class="uil uil-github"></i>
+     </a>
+
+     <a href="https://www.linkedin.com/in/constance-khalumba-0b390822a/?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base%3Bvaxi%2Fh%2BzQ1W5YjtkyuAVkw%3D%3D" className="home__social-icon" target="_blank">
+     <i class="uil uil-linkedin"></i> 
+     </a>
+   </div>
+  )
+}
+
+export default Social
