91c12024d727918e24fdcd174db4452b2fe6ae23
constance khalumba <constancekhalumba@gmail.com>
2025-04-05 17:44:39 +0300

    last commits

--- connie-portfolio-react/src/components/home/ScrollDown.jsx
+++ connie-portfolio-react/src/components/home/ScrollDown.jsx
@@ -5,11 +5,38 @@ const ScrollDown = () => {
     <div className='home__scroll'>
        <a href="#about" className="home__scroll-button 
           button--flex">
-         <svg width="70" height="74" viewBox="0 0 108 74" fill="none" xmlns="http://www.w3.org/2000/svg">
-         <rect width="108" height="74" fill="none"/>
-         <path d="M53.961 15.1368V28.9583" stroke="black" stroke-width="1.64103" strokeMiterlimit="1.5" strokeLinecap="round" strokeLinejoin="round"/>
-         <path fillRule="evenodd" clipRule="evenodd" d="M83 23.0617C83 10.8854 70.0056 1 54.0001 1C37.9944 1 25 10.8854 25 23.0617V50.9383C25 63.1146 37.9944 73 54.0001 73C70.0056 73 83 63.1146 83 50.9383V23.0617Z" stroke="var(--title-color)" stroke-width="5px" stroke-miterlimit="1.5" strokeLinecap="round" strokeLinejoin="round"/>
-          </svg>
+         <svg width="70" 
+              height="74" 
+              viewBox="0 0 108 74" 
+              fill="none" 
+              xmlns="http://www.w3.org/2000/svg"
+              xmlns:xlink="http://www.w3.org/1999/xlink"
+              style={{
+                      fillRule: "evenodd",
+                      clipRule: "evenodd",
+                      strokeLinecap: "round",
+                      strokeLinejoin: "round",
+                      strokeMiterlimit: "1.5",
+                }}
+       >
+               <path 
+                  class="wheel"
+                  d="M123.359,79.77510,72.843" 
+                  style={{
+                        fill: "none", 
+                        stroke: "#000",
+                        strokeWidth: "20px", 
+                    }}
+                ></path>
+                  <path 
+                  id="mouse"
+                  d="M236.717,123.359c0,-62.565 -50.794,-113.359 -113.
+                  358,-113.359c-62.565,0 -113.359,50.794 -113.359,113.
+                  35910,143.237c0,62.565 50.794,113.359 113.359,113.
+                  359c62.564,0 113.358" 
+                  style={{
+                  <path fillRule="evenodd" clipRule="evenodd" d="M83 23.0617C83 10.8854 70.0056 1 54.0001 1C37.9944 1 25 10.8854 25 23.0617V50.9383C25 63.1146 37.9944 73 54.0001 73C70.0056 73 83 63.1146 83 50.9383V23.0617Z" stroke="var(--title-color)" stroke-width="5px" stroke-miterlimit="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+            </svg>
           
        </a>
 </div>
