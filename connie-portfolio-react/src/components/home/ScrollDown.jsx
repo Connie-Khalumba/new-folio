893d4b0f72f736a5787565a74c80703392217186
constance khalumba <constancekhalumba@gmail.com>
2025-04-05 16:58:44 +0300

    second commits

--- connie-portfolio-react/src/components/home/ScrollDown.jsx
+++ connie-portfolio-react/src/components/home/ScrollDown.jsx
@@ -5,31 +5,13 @@ const ScrollDown = () => {
     <div className='home__scroll'>
        <a href="#about" className="home__scroll-button 
           button--flex">
-       
-          <svg
-          width="36"
-          height="36"
-          viewBox="0 0 24 24"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-          className="icon__scroll"
-        >
-        <path
-         d="M7 4V19C7 20.6569 8.34315 22 10 22H17C18.6569 22 20 20.6569 20 19V4H7Z"
-          stroke="currentColor"
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-       />
-      <path
-        d="M7 4H4V19C4 20.6569 5.34315 22 7 22V4Z"
-        stroke="currentColor"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-       />
-        </svg>
-      </a>
+         <svg width="70" height="74" viewBox="0 0 108 74" fill="none" xmlns="http://www.w3.org/2000/svg">
+         <rect width="108" height="74" fill="none"/>
+         <path d="M53.961 15.1368V28.9583" stroke="black" stroke-width="1.64103" strokeMiterlimit="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+         <path fillRule="evenodd" clipRule="evenodd" d="M83 23.0617C83 10.8854 70.0056 1 54.0001 1C37.9944 1 25 10.8854 25 23.0617V50.9383C25 63.1146 37.9944 73 54.0001 73C70.0056 73 83 63.1146 83 50.9383V23.0617Z" stroke="var(--title-color)" stroke-width="5px" stroke-miterlimit="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+          </svg>
+          
+       </a>
 </div>
   )
 }
