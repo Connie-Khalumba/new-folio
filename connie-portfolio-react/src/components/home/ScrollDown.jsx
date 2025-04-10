959479e32cab09e24466ea1b2f45c35753c22342
constance khalumba <constancekhalumba@gmail.com>
2025-04-10 15:48:09 +0300

    second commits

--- connie-portfolio-react/src/components/home/ScrollDown.jsx
+++ connie-portfolio-react/src/components/home/ScrollDown.jsx
@@ -5,45 +5,11 @@ const ScrollDown = () => {
     <div className='home__scroll'>
        <a href="#about" className="home__scroll-button 
           button--flex">
-         <svg 
-              width="32px" 
-              height="32px" 
-              class="home__scroll-mouse" 
-              viewBox="0 0 247 390" 
-              version="1.1" 
-              xmlns="http://www.w3.org/2000/svg"
-              xmlnsXlink="http://www.w3.org/1999/xlink"
-              style={{
-                      fillRule: "evenodd",
-                      clipRule: "evenodd",
-                      strokeLinecap: "round",
-                      strokeLinejoin: "round",
-                      strokeMiterlimit: "1.5",
-                    }}
-       >
-               <path 
-                  className="wheel"
-                  d="M123.359,79.77510,72.843" 
-                  style={{
-                        fill: "none", 
-                        stroke: "var(--title-color)",
-                        strokeWidth: "20px", 
-                    }}
-                ></path>
-                  <path 
-                  id="mouse"
-                  d="M236.717,123.359c0,-62.565 -50.794,-113.359 -113.
-                  358,-113.359c-62.565,0 -113.359,50.794 -113.359,113.
-                  35910,143.237c0,62.565 50.794,113.359 113.359,113.
-                  359c62.564,0 113.358,-50.794 -113.359,113.35910,-143.
-                  237Z" 
-                  style={{
-                    fill: "none", 
-                    stroke: "var(--title-color)",
-                    strokeWidth: "20px", 
-                }}
-                ></path>
-        </svg>
+         <svg width="32" height="30" viewBox="0 0 41 60" fill="none" xmlns="http://www.w3.org/2000/svg">
+           <path d="M20.9761 12.2731V23.4797" stroke="black" stroke-width="1.64103" stroke-miterlimit="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+           <path fill-rule="evenodd" clip-rule="evenodd" d="M40.1598 18.9783C40.1598 9.35291 31.5639 1.53845 20.9761 1.53845C10.3882 1.53845 1.7923 9.35291 1.7923 18.9783V41.0148C1.7923 50.6401 10.3882 58.4546 20.9761 58.4546C31.5639 58.4546 40.1598 50.6401 40.1598 41.0148V18.9783Z" stroke="black" stroke-width="1.64103" stroke-miterlimit="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+         </svg>
+
         <span className="home__scroll-name">Scroll Down</span>
          <i class="uil uil-arrow-down home__scroll-arrow"></i> 
        </a>
