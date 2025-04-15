129c952b3ced5672e6fa48a40c5f427967c8d6bc
constance khalumba <constancekhalumba@gmail.com>
2025-04-15 15:46:47 +0300

    first commit

--- connie-portfolio-react/src/components/qualification/Qualification.jsx
+++ connie-portfolio-react/src/components/qualification/Qualification.jsx
@@ -21,6 +21,164 @@ const Qualification = () => {
             </div>
 
         </div>
+
+        <div className="qualification__sections">
+            <div className="qualification__content">
+                <div className="qualification__data">
+                    <div>
+                        <h3 className='qualification__title'>Web Development</h3>
+                        <span className="qualification__subtitle">
+                        SHIELD Intl Program
+                        </span>
+                        <div className="qualification__calender">
+                            <i className="uil uil-calendar-alt"></i> March 2024 - Present
+                        </div>
+                    </div>
+
+                    <div>
+                        <span className="qualification__rounder"></span>
+                        <span className="qualification__line"></span>
+                    </div>
+                </div>
+
+                <div className="qualification__data">
+                    <div></div>
+
+                    <div>
+                        <span className="qualification__rounder"></span>
+                        <span className="qualification__line"></span>
+                    </div>
+
+                    <div>
+                        <h3 className='qualification__title'>Social Media management Intern</h3>
+                        <span className="qualification__subtitle">
+                        Atote consultants
+                        </span>
+                        <div className="qualification__calender">
+                            <i className="uil uil-calendar-alt"></i>  Jan 2024 - March 2024
+                        </div>
+                    </div>
+
+                    
+                </div>
+
+                <div className="qualification__data">
+                    <div>
+                        <h3 className='qualification__title'>Diploma in Web Development || Digital Litracy Program</h3>
+                        <span className="qualification__subtitle">
+                        Nairobits Trust
+                        </span>
+                        <div className="qualification__calender">
+                            <i className="uil uil-calendar-alt"></i> September 2021 - Dec 2023
+                        </div>
+                    </div>
+
+                    <div>
+                        <span className="qualification__rounder"></span>
+                        <span className="qualification__line"></span>
+                    </div>
+                </div>
+
+                <div className="qualification__data">
+                    <div></div>
+
+                    <div>
+                        <span className="qualification__rounder"></span>
+                        <span className="qualification__line"></span>
+                    </div>
+
+                    <div>
+                        <h3 className='qualification__title'>Certificate in Information and Technology</h3>
+                        <span className="qualification__subtitle">
+                        Kabete National Polytechnic
+                        </span>
+                        <div className="qualification__calender">
+                            <i className="uil uil-calendar-alt"></i> May 2019 - Dec 2023
+                        </div>
+                    </div>
+
+                    
+                </div>
+            </div>
+
+            <div className="qualification__content">
+                <div className="qualification__data">
+                    <div>
+                        <h3 className='qualification__title'>Web Development</h3>
+                        <span className="qualification__subtitle">
+                        SHIELD Intl Program
+                        </span>
+                        <div className="qualification__calender">
+                            <i className="uil uil-calendar-alt"></i> March 2024 - Present
+                        </div>
+                    </div>
+
+                    <div>
+                        <span className="qualification__rounder"></span>
+                        <span className="qualification__line"></span>
+                    </div>
+                </div>
+
+                <div className="qualification__data">
+                    <div></div>
+
+                    <div>
+                        <span className="qualification__rounder"></span>
+                        <span className="qualification__line"></span>
+                    </div>
+
+                    <div>
+                        <h3 className='qualification__title'>Social Media management Intern</h3>
+                        <span className="qualification__subtitle">
+                        Atote consultants
+                        </span>
+                        <div className="qualification__calender">
+                            <i className="uil uil-calendar-alt"></i>  Jan 2024 - March 2024
+                        </div>
+                    </div>
+
+                    
+                </div>
+
+                <div className="qualification__data">
+                    <div>
+                        <h3 className='qualification__title'>Diploma in Web Development || Digital Litracy Program</h3>
+                        <span className="qualification__subtitle">
+                        Nairobits Trust
+                        </span>
+                        <div className="qualification__calender">
+                            <i className="uil uil-calendar-alt"></i> September 2021 - Dec 2023
+                        </div>
+                    </div>
+
+                    <div>
+                        <span className="qualification__rounder"></span>
+                        <span className="qualification__line"></span>
+                    </div>
+                </div>
+
+                <div className="qualification__data">
+                    <div></div>
+
+                    <div>
+                        <span className="qualification__rounder"></span>
+                        <span className="qualification__line"></span>
+                    </div>
+
+                    <div>
+                        <h3 className='qualification__title'>Certificate in Information and Technology</h3>
+                        <span className="qualification__subtitle">
+                        Kabete National Polytechnic
+                        </span>
+                        <div className="qualification__calender">
+                            <i className="uil uil-calendar-alt"></i> May 2019 - Dec 2023
+                        </div>
+                    </div>
+
+                    
+                </div>
+            </div>
+        </div>
      </div>
    </section>
   )
