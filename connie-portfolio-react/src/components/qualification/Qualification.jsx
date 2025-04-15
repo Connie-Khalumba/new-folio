94f6284a788b3908610623777bb40a4ad2a2c88f
constance khalumba <constancekhalumba@gmail.com>
2025-04-15 14:48:03 +0300

    first commit

---
+++ connie-portfolio-react/src/components/qualification/Qualification.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './qualification.css'
+
+const Qualification = () => {
+  return (
+   <section className="qaulification section">
+     <h2 className="section__title">Qualification</h2>
+     <span className="section__subtitle">What I offer</span>
+   </section>
+  )
+}
+
+export default Qualification
