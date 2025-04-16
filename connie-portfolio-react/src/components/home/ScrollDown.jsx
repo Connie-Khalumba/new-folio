a2bff7d0ec1789bf65c9e682bf4085a0bb067cf5
constance khalumba <constancekhalumba@gmail.com>
2025-04-16 16:23:06 +0300

    added pictures

--- connie-portfolio-react/src/components/home/ScrollDown.jsx
+++ connie-portfolio-react/src/components/home/ScrollDown.jsx
@@ -10,7 +10,7 @@ const ScrollDown = () => {
            <path fill-rule="evenodd" clip-rule="evenodd" d="M40.1598 18.9783C40.1598 9.35291 31.5639 1.53845 20.9761 1.53845C10.3882 1.53845 1.7923 9.35291 1.7923 18.9783V41.0148C1.7923 50.6401 10.3882 58.4546 20.9761 58.4546C31.5639 58.4546 40.1598 50.6401 40.1598 41.0148V18.9783Z" stroke="black" stroke-width="1.64103" stroke-miterlimit="1.5" stroke-linecap="round" stroke-linejoin="round"/>
          </svg>
 
-        <span className="home__scroll-name">Scroll Down</span>
+         <span className="home__scroll-name">Scroll Down</span>
          <i class="uil uil-arrow-down home__scroll-arrow"></i> 
        </a>
 </div>
---
+++ connie-portfolio-react/src/components/testimonials/Data.jsx
@@ -0,0 +1,27 @@
+import Image1 from "../../assets/testimonial2.png";
+import Image2 from '../../assets/testimonial3.jpg'; 
+import Image3 from "../../assets/Hellen.jpg";
+
+export const Data = [
+    {
+        id: 1,
+        image: Image1,
+        title: "Cate",
+        description:
+          "A really good job, all aspects of the project were followed step by step and with good results.",
+      },
+      {
+        id: 2,
+        image: Image2,
+        title: "Jane Branice",
+        description:
+          "A really good job, all aspects of the project were followed step by step and with good results.",
+      },
+      {
+        id: 3,
+        image: Image3,
+        title: "Hellen Mwania",
+        description:
+          "A really good job, all aspects of the project were followed step by step and with good results.",
+      },
+];
