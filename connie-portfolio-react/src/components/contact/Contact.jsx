fcd204bdd9fb17b90dacb6d2e7303fae715a3865
constance khalumba <constancekhalumba@gmail.com>
2025-04-30 16:35:29 +0300

    second commits

--- connie-portfolio-react/src/components/contact/Contact.jsx
+++ connie-portfolio-react/src/components/contact/Contact.jsx
@@ -53,21 +53,21 @@ const Contact = () => {
                 <h3 className="contact__title">Write me your project</h3>
 
                 <form className="contact__form">
-                  <div className="conact__form-div">
+                  <div className="contact__form-div">
                     <label className="contact__form-tag"> Name</label>
                     <input type="text" name="name" 
                     className="contact__form-input"
                     placeholder="Insert your name"/>
                   </div>
 
-                  <div className="conact__form-div">
+                  <div className="contact__form-div">
                     <label className="contact__form-tag"> Mail</label>
                     <input type="email" name="email" 
                     className="contact__form-input"
                     placeholder="Insert your email"/>
                   </div>
 
-                  <div className="conact__form-div">
+                  <div className="contact__form-div">
                     <label className="contact__form-tag"> Project</label>
                     <textarea name="project" cols="30" rows="10"
                     className="contact__form-input" 
