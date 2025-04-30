a03a2c41e889b96604c993e35f6b673cfa3d9ad5
constance khalumba <constancekhalumba@gmail.com>
2025-04-30 16:23:07 +0300

    second commits

--- connie-portfolio-react/src/components/contact/Contact.jsx
+++ connie-portfolio-react/src/components/contact/Contact.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import './contact.css';
 
 const Contact = () => {
   return (
@@ -50,6 +51,56 @@ const Contact = () => {
 
             <div className="contact__content">
                 <h3 className="contact__title">Write me your project</h3>
+
+                <form className="contact__form">
+                  <div className="conact__form-div">
+                    <label className="contact__form-tag"> Name</label>
+                    <input type="text" name="name" 
+                    className="contact__form-input"
+                    placeholder="Insert your name"/>
+                  </div>
+
+                  <div className="conact__form-div">
+                    <label className="contact__form-tag"> Mail</label>
+                    <input type="email" name="email" 
+                    className="contact__form-input"
+                    placeholder="Insert your email"/>
+                  </div>
+
+                  <div className="conact__form-div">
+                    <label className="contact__form-tag"> Project</label>
+                    <textarea name="project" cols="30" rows="10"
+                    className="contact__form-input" 
+                    placeholder='write your project'></textarea>
+                  </div>
+
+                  <button href="#contact" className="button button--flex">
+                  Send Message
+                  <svg
+         width="36"
+         height="36"
+         viewBox="0 0 24 24"
+         fill="none"
+         xmlns="http://www.w3.org/2000/svg"
+         className="icon__send"
+        >
+       <path
+          d="M22 2L11 13"
+          stroke="currentColor"
+          stroke-width="2"
+         stroke-linecap="round"
+         stroke-linejoin="round"
+       />
+       <path
+         d="M22 2L15 22L11 13L2 9L22 2Z"
+         stroke="currentColor"
+         stroke-width="2"
+         stroke-linecap="round"
+         stroke-linejoin="round"
+       />
+                 </svg>
+                   </button>
+                </form>
             </div>
         </div>
     </section>
