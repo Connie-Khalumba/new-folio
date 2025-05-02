4b41107321d24fdbf2aecb05365082ed27c698ca
constance khalumba <constancekhalumba@gmail.com>
2025-05-02 16:58:10 +0300

    changes in assets

--- connie-portfolio-react/src/components/work/Data.jsx
+++ connie-portfolio-react/src/components/work/Data.jsx
@@ -3,11 +3,7 @@ import Work2 from "../../assets/work2.jpg";
 import Work3 from "../../assets/work3.jpg";
 import Work4 from "../../assets/work4.jpg";
 import Work5 from "../../assets/work5.jpg";
-import Work6 from "../../assets/work6.jpg";
-import Work7 from "../../assets/work7.jpg";
-import Work8 from "../../assets/work8.jpg";
-import Work9 from "../../assets/work9.jpg";
-import Work10 from "../../assets/work10.jpg";
+
 
 export const projectsData = [
     {
@@ -37,39 +33,9 @@ export const projectsData = [
     {
       id: 5,
       image: Work5,
-      title: "Web development",
+      title: "Web design",
       category: "web",
     },
-    {
-        id: 6,
-        image: Work6,
-        title: "Web design",
-        category: "web",
-      },
-      {
-        id: 7,
-        image: Work7,
-        title: "Web design",
-        category: "web",
-      },
-      {
-        id: 8,
-        image: Work8,
-        title: "Web design",
-        category: "web",
-      },
-      {
-        id: 9,
-        image: Work9,
-        title: "Web design",
-        category: "web",
-      },
-      {
-        id: 10,
-        image: Work10,
-        title: "Web design",
-        category: "web",
-      },
   ];
 
   export const projectsNav = [
---
+++ connie-portfolio-react/src/components/work/WorkItems.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const WorksItems = (item) => {
+  return (
+   <div className="work__card" key={item.id}>
+     <img src={item.image} alt="" className='work__img' />
+
+   </div>
+  )
+}
+
+export default WorksItems
