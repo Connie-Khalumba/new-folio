540ec0a431980c35d3a75084efb41467a0012d16
constance khalumba <constancekhalumba@gmail.com>
2025-05-03 14:44:03 +0300

    first commits saturday 3rd

--- connie-portfolio-react/src/components/work/Works.jsx
+++ connie-portfolio-react/src/components/work/Works.jsx
@@ -1,51 +1,49 @@
 import React, { useEffect, useState } from 'react'
-import {projectsData} from './Data'
-import { projectsNav } from './Data'
+import { projectsData, projectsNav } from './Data'
 import WorkItems from './WorkItems'
 
 const Works = () => {
-  const [item, setItem] = useState({ name: 'all'});
+  const [item, setItem] = useState({ name: 'all' });
   const [projects, setProjects] = useState([]);
-  const [active, setActive] = useState(0)
+  const [active, setActive] = useState(0);
 
   useEffect(() => {
-    if(item.name === "all") {
-      setProjects(projectsData)
-    }
-
-    else {
-      const newProjects = projectsData.filter((project) => {
-        return project.category === item.name;
-      });
-      setProjects(newProjects)
+    if (item.name === 'all') {
+      setProjects(projectsData);
+    } else {
+      const newProjects = projectsData.filter((project) =>
+        project.category === item.name
+      );
+      setProjects(newProjects);
     }
   }, [item]);
 
   const handleClick = (e, index) => {
-    setItem({name: e.target.textContent });
+    setItem({ name: e.target.textContent });
+    setActive(index);
   };
+
   return (
     <div>
       <div className="work__filters">
-      {projects.map((item, index) => {
-        return (
-          <span onClick={(e) => {
-            handleClick(e, index)
-          }}
-          className={`${active === index ? 'active-work' : ""}
-          work__item`}
-          key={index}>{item.name}</span>
-        )
-      })}
-    </div>
+        {projectsNav.map((item, index) => (
+          <span
+            onClick={(e) => handleClick(e, index)}
+            className={`${active === index ? 'active-work' : ''} work__item`}
+            key={index}
+          >
+            {item.name}
+          </span>
+        ))}
+      </div>
 
-    <div className="work__container container grid">
-      {projectsData.flatMap((item) => {
-        return <WorkItems item={item} key={item.id}/>
-      })}
+      <div className="work__container container grid">
+        {projects.map((item) => (
+          <WorkItems item={item} key={item.id} />
+        ))}
+      </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default Works
+export default Works;
