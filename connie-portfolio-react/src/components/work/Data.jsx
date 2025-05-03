1e0c5c3634f328b82b58e3bc3272a86423b9539c
constance khalumba <constancekhalumba@gmail.com>
2025-05-03 15:23:02 +0300

    last commits on the images adjustments

--- connie-portfolio-react/src/components/work/Data.jsx
+++ connie-portfolio-react/src/components/work/Data.jsx
@@ -3,38 +3,74 @@ import Work2 from "../../assets/work2.jpg";
 import Work3 from "../../assets/work3.jpg";
 import Work4 from "../../assets/work4.jpg";
 import Work5 from "../../assets/work5.jpg";
+import Work6 from "../../assets/work6.jpg";
+import Work7 from "../../assets/work7.jpg";
+import Work8 from "../../assets/work8.jpg";
+import Work9 from "../../assets/work9.jpg";
+import Work10 from "../../assets/work10.jpg";
+
 
 
 export const projectsData = [
     {
       id: 1,
       image: Work1,
-      title: "Web design",
+      title: "Blackjak Technologies",
       category: "web",
     },
     {
       id: 2,
       image: Work2,
-      title: "App movil",
-      category: "app",
+      title: "Tuzuru Africa",
+      category: "web",
     },
     {
       id: 3,
       image: Work3,
-      title: "Brand design",
-      category: "design",
+      title: "Weather App",
+      category: "app",
     },
     {
       id: 4,
       image: Work4,
-      title: "App movie",
+      title: "Redifu App",
       category: "app",
     },
     {
       id: 5,
       image: Work5,
-      title: "Web design",
+      title: "Kharl agency",
       category: "web",
+    },
+    {
+      id: 6,
+      image: Work6,
+      title: "movie site",
+      category: "web",
+    },
+    {
+      id: 7,
+      image: Work7,
+      title: "Task tracker",
+      category: "app",
+    },
+    {
+      id: 8,
+      image: Work8,
+      title: "Ecommerce",
+      category: "design",
+    },
+    {
+      id: 9,
+      image: Work9,
+      title: "Calculator",
+      category: "web",
+    },
+    {
+      id: 10,
+      image: Work10,
+      title: "Scheduler",
+      category: "design",
     },
   ];
 
